ef769ec60368b3dedf354fcae0271f5e86d5efde
SmefDev <SmefDev@gmail.com>
2023-12-26 02:26:30 +0700

    [build] update and delete todo

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,14 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task}) => {
+export const Todo = ({ task, toogleComplete, deleteTodo, editTodo }) => {
   return (
-	<div className='Todo'>
-    <p>{task.task}</p>
-    <div>
-      <FontAwesomeIcon icon={faPenToSquare} />
-      <FontAwesomeIcon icon={faTrash} />
+    <div className="Todo">
+      <p
+        onClick={() => toogleComplete(task.id)}
+        className={`${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FontAwesomeIcon
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
