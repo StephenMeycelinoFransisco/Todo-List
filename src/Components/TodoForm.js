1f8d1823fcfb3cb2bff7f5e44e5f2a6519e4226a
SmefDev <SmefDev@gmail.com>
2023-12-26 02:04:46 +0700

    [build] add list todo

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTodo(value)
+
+    setValue("")
+  }
+  return (
+	<form className='TodoForm' onSubmit={handleSubmit}>
+    <input type='text' value={value} onChange={(e) => setValue(e.target.value) } className='todo-input' placeholder='What is the taks today' />
+    <button type='submit' className='todo-btn'>Add Taks</button>
+  </form>
+  )
+}
